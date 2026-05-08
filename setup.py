f10d0a6c37f1e6f1b76d6d0ecbca69bbe9b2b614
basawaraj-fosserp <103717083+basawaraj-fosserp@users.noreply.github.com>
2026-05-08 16:33:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in bonito_customizations/__init__.py
-from bonito_customizations import __version__ as version
+#from bonito_customizations import __version__ as version
 
 setup(
 	name="bonito_customizations",
