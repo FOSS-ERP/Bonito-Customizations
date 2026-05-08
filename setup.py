a42a2d9bea8099157dd26a3afe38b2332efc7bda
basawaraj-fosserp <103717083+basawaraj-fosserp@users.noreply.github.com>
2026-05-08 16:38:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
-
-# get version from __version__ variable in bonito_customizations/__init__.py
-#from bonito_customizations import __version__ as version
-
 setup(
-	name="bonito_customizations",
-	version=version,
-	description="Bonito Customizations",
-	author="Bonito Designs",
-	author_email="info@bonito.in",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+    name="bonito_customizations",
+    version="0.0.1",
+    description="Bonito Customizations",
+    author="Your Name",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
 )
