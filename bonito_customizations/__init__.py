9f8974898b8f8067068d269a6b4cb45e19565777
sathwikguduguntla-Foss <sathwik.guduguntla@fosserp.com>
2026-08-29 18:59:32 +0530

    feat:correction monkey patch

--- bonito_customizations/__init__.py
+++ bonito_customizations/__init__.py
@@ -1,11 +1,13 @@
 __version__ = "0.0.1"
 
-from india_compliance.gst_india.utils import e_invoice
-from india_compliance.gst_india.utils import e_waybill
+from india_compliance.gst_india.utils import original_e_invoice
+from india_compliance.gst_india.utils import original_e_waybill
+
+
 
 # Apply custom overrides
 from bonito_customizations.e_invoice_override import apply_e_invoice_override
 from bonito_customizations.e_waybill_override import apply_e_waybill_override
 
-apply_e_invoice_override()
-apply_e_waybill_override()
+original_e_invoice.e_invoice = apply_e_invoice_override
+original_e_waybill = apply_e_invoice_override
